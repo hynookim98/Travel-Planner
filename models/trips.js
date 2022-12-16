2d7f7295b3f4e9dc4f6f6ac649af5d3b2718f2a1
hynookim98 <hynookim98@gmail.com>
2022-12-16 13:44:10 -0800

    finished trips.js model

---
+++ models/trips.js
@@ -0,0 +1,47 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config');
+
+class Trips extends Model {}
+
+Trips.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        trip_budget: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
+        traveller_amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1
+        },
+        traveller_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "traveller", // *model name
+                key: 'id',
+                // unique: false
+            }
+        },
+        location_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "location",
+                key: 'id',
+                unique: false
+            }
+        }
+    },
+    {
+        sequelize,
+        underscored: true,
+        modelName: 'trips'
+    }
+);
+
+module.exports = Trips;
